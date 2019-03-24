d28de13be00f8ded290d97c17c938a0366050890
scahalan <13sc77@queensu.ca>
2019-03-24 15:58:22 -0400

    add main info content

--- info/info.php
+++ info/info.php
@@ -26,6 +26,84 @@
 
         <div class="content">
 
+        <?php 
+            include '../util/DBController.php';
+            $db_handle = new DBController();
+
+            $numAttendees = $db_handle->runQuery("SELECT COUNT(id) as num FROM attendee")[0]['num'];
+            $numStudents = $db_handle->runQuery("SELECT COUNT(id) as num FROM student ")[0]['num'];
+            $numProfessionals = $db_handle->runQuery("SELECT COUNT(id) as num FROM professional ")[0]['num'];
+
+            $attendanceSum = $db_handle->runQuery("SELECT SUM(price) as sum FROM attendee")[0]['sum'];
+            
+
+            $sponsorships = $db_handle->runQuery("SELECT st.class, cost, COUNT(st.class), SUM(cost)
+                FROM sponsor_company sc INNER JOIN sponsor_type st ON sc.class = st.class
+                GROUP BY st.class");
+            $sponsorSum = $db_handle->runQuery("SELECT SUM(cost) as sum
+                FROM sponsor_company sc 
+                INNER JOIN sponsor_type st ON sc.class = st.class")[0]['sum'];
+
+        ?>
+
+        <table>
+            <tr>
+                <td style="text-align: center;" colspan="4">Attendance</td>
+            </tr>
+            <tr>
+                <td>Attendee type</td>
+                <td>Ticket cost</td>
+                <td>Number of attendees</td>
+                <td>Total amount</td>
+            </tr>
+            <tr>
+                <td>Student</td>
+                <td>50</td>
+                <td><?php echo $numStudents;?></td>
+                <td><?php echo $numStudents * 50;?></td>
+            </tr>
+            <tr>
+                <td>Professional</td>
+                <td>100</td>
+                <td><?php echo $numProfessionals;?></td>
+                <td><?php echo $numProfessionals * 100;?></td>
+            </tr>
+            <tr>
+                <td></td><td></td><td></td>
+                <td><?php echo $attendanceSum; ?></td>
+            </tr>
+
+            <tr>
+                <td style="text-align: center;" colspan="4">Sponsorship</td>
+            </tr>
+
+
+            <tr>
+                <td>Sponsor type</td>
+                <td>Price</td>
+                <td>Number of sponsorships</td>
+                <td>Total amount</td>
+            </tr>
+
+            <?php 
+                foreach($sponsorships as $row){
+                    echo "<tr>";
+                    foreach($row as $col){
+                        echo "<td>";
+                        echo $col;
+                        echo "</td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+            <tr>
+                <td></td><td></td><td></td>
+                <td><?php echo $sponsorSum; ?></td>
+            </tr>
+            
+        </table>
+
+
         </div>
 
         <script type="text/javascript" src="/332demo/index.js"></script>
