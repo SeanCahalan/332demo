a86a93ab36e5a67ce1302325ba5d76a75274eaa9
James Ford <jackamo96@gmail.com>
2019-03-26 17:26:22 -0400

    Refactored print table

--- util/PrintTable.php
+++ util/PrintTable.php
@@ -1,15 +1,17 @@
 <?php 
-    function printTable($headers, $stmt){
-        require_once('TableRows.php');
-
+    function printTable($headers, $values){
         echo "<table style='border: solid 1px black;'>";
         foreach($headers as $header){
             echo "<th>$header</th>";
         }
         echo "</tr>";
 
-        foreach(new TableRows(new RecursiveArrayIterator($stmt)) as $k=>$v) { 
-            echo $v;
+        foreach ($values as $row) {
+            echo "<tr>";
+            foreach ($row as $stmt){
+                echo '<td style="width:150px;border:1px solid black;">'.$stmt."</td>";
+            }
+            echo "</tr>";
         }
 
         echo "</table>";
